f2aa9b0e14ca52e2788b3079135df7847eb27117
Eugene Zaznoba <irishjimdegriz@gmail.com>
2020-06-09 23:22:35 +0300

    feat: operations with variables

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
- let money;
- let income;
- let addExpenses;
- let deposit;
- let mission;
- let period;
+let money = 3000;
+let income = 'супергерой на полставки';
+let addExpenses = 'Электричество, интернеТ, праЧЕЧная';
+let deposit = true;
+let mission = '10000';
+let period = 5;
 
- alert("Привет, мир!");
- console.log("Hello world!");
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " долларов");
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+
+console.log(budgetDay);
+
+//  alert("Привет, мир!");
+//  console.log("Hello world!");
