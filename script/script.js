970ac0892ce255e5be3c27b7bd52f98d77014d22
Eugene Zaznoba <irishjimdegriz@gmail.com>
2020-06-30 19:07:39 +0300

    feat: almost done task in video

--- script/script.js
+++ script/script.js
@@ -4,6 +4,7 @@ window.addEventListener('DOMContentLoaded', () =>{
         timerMinutes = document.querySelector('#timer-minutes'),
         timerSeconds = document.querySelector('#timer-seconds');
 
+  //таймер
   const countTimer = (deadline) => {
 
     const getTimeRemaining = () => {
@@ -32,4 +33,43 @@ window.addEventListener('DOMContentLoaded', () =>{
     };
 
   countTimer('01 July 2020');
+
+  //Меню
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector('.menu'),
+          menu = document.querySelector('menu'),
+          closeBtn = document.querySelector('.close-btn'),
+          menuItems = menu.querySelectorAll('ul>li');
+
+    const handlerMenu = () => {
+      menu.classList.toggle('active-menu');
+    };
+
+    btnMenu.addEventListener('click', handlerMenu);
+    closeBtn.addEventListener('click', handlerMenu);
+
+    menuItems.forEach((item) => item.addEventListener('click', handlerMenu));
+  };
+
+  toggleMenu();
+
+  //popup
+
+  const togglePopup = () => {
+    const popup = document.querySelector('.popup'),
+          popupBtns = document.querySelectorAll('.popup-btn'),
+          popupClose = document.querySelector('.popup-close');
+
+    popupBtns.forEach((item) => {
+      item.addEventListener('click', () => {
+        popup.classList.display = 'block';
+      });
+    });
+
+    popupClose.addEventListener('click', () => {
+      popup.classList.display = 'none';
+    });
+  };
+
+  togglePopup();
 })
